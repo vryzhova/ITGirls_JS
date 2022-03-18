e4fbf7ed40d6f88cf07350e80f8ee800509eba85
Valeria Ryzhova <ryzhova_valieriia@mail.ru>
2022-03-18 16:27:03 +0300

    add gallary

---
+++ week14/gallary/script.js
@@ -0,0 +1,8 @@
+function forward() {
+    let change = document.getElementById('anime');
+    change.src = "assets/2.jpg";
+}
+function back() {
+    let change = document.getElementById('anime');
+    change.src = "assets/1.jpg";
+}
