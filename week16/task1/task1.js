7fec83db91224388031308ab8c990725eec2d545
Valeria Ryzhova <ryzhova_valieriia@mail.ru>
2022-03-30 19:19:20 +0300

    fix

--- week16/task1/task1.js
+++ week16/task1/task1.js
@@ -4,25 +4,8 @@ function sumInput() {
     for (;;) {
         let value = +prompt("Введите число", '');
         if (!value ) break;
-        arr.push(value);
-        arr.sort();
-        console.log(arr)
+        arr.push(value); 
         sum += value;
     }
-    alert('Отсортированный массив: ' + arr.sort() + ' ' + 'Сумма элементов: ' + sum )
-
-    
+    alert('Отсортированный массив: ' + arr.sort() + ' ' + 'Сумма элементов: ' + sum );   
 }
-
-// let sum = 0;
-
-// for(;;) {
-
-//   let value = +prompt("Введите число", '');
-
-//   if (!value) break; 
-
-//   sum += value;
-
-// }
-// alert( 'Сумма: ' + sum );
