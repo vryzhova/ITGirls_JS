1e26982fe00d9a9415571b0e3738fadc0e61adb3
Valeria Ryzhova <ryzhova_valieriia@mail.ru>
2022-04-10 14:57:47 +0300

    add task 2 week 17

---
+++ week17/task2/task2.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.btn');
+let comments =['hi','hello'];
+
+document.addEventListener("DOMContentLoaded",function(){
+    generateComments()
+})
+
+function checkSpam(){
+    const text = document.querySelector('.comment_area').value;
+    let newText = text.replace(/viagra/i && /xxx/i , '***');
+    comments.push(newText);
+}
+
+function generateComments() {
+    let optionString = "";
+    for (let comment of comments){
+    optionString += `<div class= "comment">${comment}</div>`;
+    }
+    document.querySelector('.comments').innerHTML = optionString;
+}
+
+btn.addEventListener('click', function(){
+    const text = document.querySelector('.comment_area').value;
+    console.log(comments);
+    checkSpam();
+    generateComments();
+})
